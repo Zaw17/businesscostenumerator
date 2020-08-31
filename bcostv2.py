c19fa25ebde01d735aeec11ba63ab6cc1c80a7ab
Myo Khant Zaw <48384211+Zaw17@users.noreply.github.com>
2020-08-31 16:12:44 +0630

    second updated version

---
+++ bcostv2.py
@@ -0,0 +1,43 @@
+import os 
+import random
+
+print("This is an automated program to determine the business costs")
+
+print("Input the fixed costs in dollars")
+
+num1 = input()
+
+num1 = int(num1)
+
+print("Input the total variable costs in dollars")
+
+num2 = input()
+
+num2 = int(num2)
+
+print("The amount of units produced")
+
+num3 = input()
+
+num3 = int(num3)
+
+print("The selling price in dollars")
+
+num4 = input()
+
+num4 = int(num4)
+
+print(f'The total cost of production is ${num1 + num2}')
+
+print(f'The variable cost per unit is ${num2/num3}')
+
+print(f'The unitary cost is ${(num1+num2)/num3}')
+
+print(f'The break even unit is {num1/(num4-(num2/num3))}')
+
+print(f'The break even revenue is ${(num1/(num4-(num2/num3)))* num4}')
+
+print(f'Your profit in percents is {(((num4-((num1+num2)/num3))/((num1+num2)/num3)*100))}%')
+
+print(f'The recommended selling price accounting for the total costs and profit of 40% is ${1.4*((num1+num2)/num3)}')
+
